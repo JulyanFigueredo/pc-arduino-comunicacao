f047a49292dae956f867aade17ba2362ca259343
Julyan <julyan_figueredo@hotmail.com>
2018-06-11 20:42:59 -0300

    bytes chegam no shiftregister

--- serial_teste.py
+++ serial_teste.py
@@ -1,24 +1,16 @@
 # This Python file uses the following encoding: utf-8
 
 import serial
-import numpy
+import time
 
-ser = serial.Serial(port='/dev/ttyACM1', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2) 
+
+ser = serial.Serial(port='/dev/ttyACM2', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2) 
 try:
     ser.isOpen()
     print("Conexão serial aberta")
 except:
     print("Erro")
-    exit()
-
-# if(ser.isOpen):
-#     try:
-#         while(1):
-#             print(ser.read())
-#     except Exception:
-#             print("erro")
-# else:
-#     ("A porta serial não abre")       
+    exit() 
 
 LINHAS = 8 
 COLUNAS = 4
@@ -30,7 +22,18 @@ ORDEM = 3
 # mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E') 
 
 def LimparMatriz():
-    mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E')
+    # mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E')
+    
+    mensagem = [[0 for i in range(PLACAS)] for j in range(LINHAS)]
+    mensagem = [[b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
+    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'] ]
+    
     print(mensagem)
     return mensagem
 
@@ -40,7 +43,7 @@ def ConfigurarMatriz(mensagem):
     j=0
     print(mensagem)
     print('DIGITE A CONFIGURAÇÃO DA MATRIZ (24 linhas por 12 colunas)');
-    for i in xrange (0, LINHAS_MATRIZ):
+    for i in range (LINHAS_MATRIZ):
         coluna = input('LINHA '+  repr(i) + ", COLUNA:") # de 0 a 11, tem 12 colunas
         # tendo a linha i, descobrir em qual pilha(0, 1 ou 2) de placas está essa linha
         pilha = i/LINHAS;
@@ -69,20 +72,30 @@ def MandarDados(mensagem):
     i=0
     j=0
     mensagem = MontarExemplo(mensagem)
-    print("")
-    print(mensagem)
-    for i in xrange (0, LINHAS):
-        for j in xrange (0, PLACAS):
-            ser.write(chr(255))
-            ser.write(chr(mensagem[i][j]))
-            print(hex(mensagem[i][j]))
-        ser.write('A')
+
+    for i in range (LINHAS):
+        for j in range (PLACAS-1,-1,-1):
+            print("linha ",i, "placa ", j)
+            ser.write(b'\xff')
+            ser.write(mensagem[i][j]) 
+            time.sleep(1)       
+            # return mensagem
+        print("fafafaf")    
+        return mensagem
+        ser.write(b'\x65')
         print(ser.read(1))
     return mensagem
 
-def MontarExemplo(mensagem):
-    # mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E') 
-    mensagem = (bytearray(b'\x10\x00\x00\x00\x10\x00\x00\x00\x10'), bytearray(b'B\x02\x02\x02B\x02\x02\x02B'), bytearray(b'D\x04\x04\x04D\x04\x04\x04D'), bytearray(b'&\x06\x06\x06&\x06\x06\x06&'), bytearray(b'H\x08\x08\x08H\x08\x08\x08H'), bytearray(b'*\n\n\n*\n\n\n*'), bytearray(b'L\x0c\x0c\x0cL\x0c\x0c\x0c,'), bytearray(b'\x1e\x0e\x0e\x0e\x1e\x0e\x0e\x0e\x1e'))
+def MontarExemplo(mensagem):    
+    mensagem = [[b'\xFF',b'\x11',b'\xFF',b'\x11',b'\x18',b'\x11',b'\x11',b'\x11',b'\x16'],
+    [b'\x08',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
+    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\x10'],]
+    
     print(mensagem)
     return mensagem
 
@@ -92,7 +105,7 @@ def SAIR():
     return 
 
 while 1:
-    val = raw_input("0 - Montar Exemplo  1 - Limpar Matriz  2 - Configurar Matriz  3 - Mandar Dados  4 - SAIR: ")
+    val = input("0 - Montar Exemplo  1 - Limpar Matriz  2 - Configurar Matriz  3 - Mandar Dados  4 - SAIR: ")
     if ( val == '1' ): m = LimparMatriz()
     if ( val == '2' ): m = ConfigurarMatriz(m)
     if ( val == '3' ): m = MandarDados(m)
