4d28a4046dd6460a3d996c8b698b2bddeee18083
Julyan <julyan_figueredo@hotmail.com>
2018-06-12 20:02:35 -0300

    aparentemente todas as linhas são acionadas

--- serial_teste.py
+++ serial_teste.py
@@ -4,9 +4,10 @@ import serial
 import time
 
 
-ser = serial.Serial(port='/dev/ttyACM2', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2) 
+ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE, timeout=2) 
 try:
     ser.isOpen()
+    time.sleep(2)
     print("Conexão serial aberta")
 except:
     print("Erro")
@@ -19,83 +20,68 @@ COLUNAS_MATRIZ = 12
 PLACAS = 9
 ORDEM = 3 
 
-# mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a\x0a'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E') 
-
 def LimparMatriz():
-    # mensagem = bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00'),bytearray(b'\x02\x02\x02\x02\x02\x02\x02\x02\x02'),bytearray(b'\x04\x04\x04\x04\x04\x04\x04\x04\x04'),bytearray(b'\x06\x06\x06\x06\x06\x06\x06\x06\x06'),bytearray(b'\x08\x08\x08\x08\x08\x08\x08\x08\x08'),bytearray(b'\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A\x0A'),bytearray(b'\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C\x0C'),bytearray(b'\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E')
-    
     mensagem = [[0 for i in range(PLACAS)] for j in range(LINHAS)]
     mensagem = [[b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'],
-    [b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00',b'\x00'] ]
+    [b'\x02',b'\x02',b'\x02',b'\x02',b'\x02',b'\x02',b'\x02',b'\x02',b'\x02'],
+    [b'\x04',b'\x04',b'\x04',b'\x04',b'\x04',b'\x04',b'\x04',b'\x04',b'\x04'],
+    [b'\x06',b'\x06',b'\x06',b'\x06',b'\x06',b'\x06',b'\x06',b'\x06',b'\x06'],
+    [b'\x08',b'\x08',b'\x08',b'\x08',b'\x08',b'\x08',b'\x08',b'\x08',b'\x08'],
+    [b'\x0a',b'\x0a',b'\x0a',b'\x0a',b'\x0a',b'\x0a',b'\x0a',b'\x0a',b'\x0a'],
+    [b'\x0c',b'\x0c',b'\x0c',b'\x0c',b'\x0c',b'\x0c',b'\x0c',b'\x0c',b'\x0c'],
+    [b'\x0e',b'\x0e',b'\x0e',b'\x0e',b'\x0e',b'\x0e',b'\x0e',b'\x0e',b'\x0e'] ]
     
-    print(mensagem)
+    ser.write(b'\x42') # apagar a matriz física
     return mensagem
 
 def ConfigurarMatriz(mensagem):
     # o vetor coluna[] recebe 24 valores para as 24 linhas da matriz desse sistema
-    i=0
-    j=0
-    print(mensagem)
-    print('DIGITE A CONFIGURAÇÃO DA MATRIZ (24 linhas por 12 colunas)');
+    print('DIGITE A CONFIGURAÇÃO DA MATRIZ (24 linhas por 12 colunas)')
     for i in range (LINHAS_MATRIZ):
-        coluna = input('LINHA '+  repr(i) + ", COLUNA:") # de 0 a 11, tem 12 colunas
-        # tendo a linha i, descobrir em qual pilha(0, 1 ou 2) de placas está essa linha
-        pilha = i/LINHAS;
-        print('pilha: ' + repr(pilha))
+        coluna = int(input('LINHA '+repr(i)+ ", COLUNA:")) # de 0 a 11, tem 12 colunas
+        if((coluna>-1)and(coluna<11)):
+            # coluna = int(input('LINHA '+repr(i)+ ", COLUNA:"))
+            # tendo a linha i, descobrir em qual pilha(0, 1 ou 2) de placas está essa linha
+            pilha = int(i/LINHAS)
+            print('pilha: ' + repr(pilha))
 
-        #descobrir a linha da placa, usado na matriz de bytes
-        linha_da_placa = i%LINHAS;
-        print("linha da placa: " + repr(linha_da_placa))
+            # descobrir a linha da placa, usado na matriz de bytes
+            linha_da_placa = i % LINHAS
+            print("linha da placa: " + repr(linha_da_placa))
 
-        #descobrir a coluna da placa, usado na matriz de bytes
-        coluna_da_placa = int(coluna%COLUNAS);
-        print("coluna da placa: "+repr(coluna_da_placa))
+            # descobrir a coluna da placa, usado na matriz de bytes
+            coluna_da_placa = (int(coluna)%COLUNAS)
+            print("coluna da placa: ", coluna_da_placa)
 
-        #descobrir o nº da placa(de 0 a 8)
-        n_placa = int(pilha*ORDEM + coluna/4);
-        print("número placa :"+ repr(n_placa))
+            # descobrir o nº da placa(de 0 a 8)
+            n_placa = int(pilha*ORDEM + coluna/4)
+            print("número placa :", n_placa)
 
-        mensagem[linha_da_placa][n_placa] |= 0x01 << (7-coluna_da_placa); 
-        print(">> " + "{0:b}".format(mensagem[linha_da_placa][n_placa]))  
-        print(">> ""{0:x}".format(mensagem[linha_da_placa][n_placa]))
+            mb = int.from_bytes(mensagem[linha_da_placa][n_placa],byteorder='big',signed=False)
+            mb |= (1 << (7-coluna_da_placa))
+            mensagem[linha_da_placa][n_placa] = mb.to_bytes(1, byteorder='big',signed=False)
+            
+        print(">> " + "{0:b}".format(mb))  
+        print(">> ""{0:x}".format(mb))
         print(mensagem[linha_da_placa][n_placa])
-    print(hex(mensagem)) 
+    print(mensagem)
     return mensagem
 
 def MandarDados(mensagem):
-    i=0
-    j=0
-    mensagem = MontarExemplo(mensagem)
-
     for i in range (LINHAS):
         for j in range (PLACAS-1,-1,-1):
             print("linha ",i, "placa ", j)
+            time.sleep(0.5)
             ser.write(b'\xff')
-            ser.write(mensagem[i][j]) 
-            time.sleep(1)       
-            # return mensagem
-        print("fafafaf")    
-        return mensagem
-        ser.write(b'\x65')
-        print(ser.read(1))
+            ser.write(mensagem[i][j])
+            print(mensagem[i][j])
+            time.sleep(0.5)
+        ser.write(b'\x41')
+        time.sleep(0.5)
     return mensagem
 
 def MontarExemplo(mensagem):    
-    mensagem = [[b'\xFF',b'\x11',b'\xFF',b'\x11',b'\x18',b'\x11',b'\x11',b'\x11',b'\x16'],
-    [b'\x08',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF'],
-    [b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\xFF',b'\x10'],]
-    
+    mensagem = [[b' ', b'\x00', b'\x00', b'\x10', b'\x00', b'\x00', b'\x00', b'@', b'\x00'], [b'\x82', b'\x02', b'\x02', b'"', b'\x02', b'\x02', b'"', b'\x02', b'\x02'], [b'\x84', b'\x04', b'\x04', b'\x84', b'\x04', b'\x04', b'\x84', b'\x04', b'\x04'], [b'F', b'\x06', b'\x06', b'\x06', b'\x86', b'\x06', b'\x06', b'&', b'\x06'], [b'H', b'\x08', b'\x08', b'\x08', b'\x88', b'\x08', b'\x08', b'(', b'\x08'], [b'*', b'\n', b'\n', b'*', b'\n', b'\n', b'*', b'\n', b'\n'], [b'\x8c', b'\x0c', b'\x0c', b'\x8c', b'\x0c', b'\x0c', b'\x0c', b'\x0c', b'\x0c'], [b'\x1e', b'\x0e', b'\x0e', b'\x0e', b'N', b'\x0e', b'\x0e', b'\x0e', b'\x0e']]
     print(mensagem)
     return mensagem
 
